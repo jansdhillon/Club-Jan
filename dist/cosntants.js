a0955ce102b58c8e0eb2b050db568ccf2eeb5980
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-08-20 15:53:54 -0700

    changing project from ground up, add new README

--- dist/cosntants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.__prod__ = void 0;
- == process.env.NODE_ENV == 'production';
-//# sourceMappingURL=cosntants.js.map
